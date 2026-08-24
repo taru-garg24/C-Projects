8d1c7a0e6c7e9fa7d100fc3842f18e6f843eccba
Taru <tarugg24@gmail.com>
2026-08-25 00:30:39 +0530

    Add C programs

---
+++ area-rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int area, peri, length, breadth;
+    printf("enter length of the rectangle: ");
+    scanf("%d", &length);
+    printf("enter breadth of the recangle: ");
+    scanf("%d", &breadth);
+    area=length*breadth;
+    peri=2*(length+breadth);
+    printf("area of rectangle is: %d\n", area);
+    printf("perimeter of rectangle is: %d\n", peri);
+    return 0;
+}
