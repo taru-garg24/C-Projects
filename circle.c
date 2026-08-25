e26f567c761b8a13e3df306c4b68de101d2a8913
Taru <tarugg24@gmail.com>
2026-08-26 00:14:27 +0530

    Add all programs

---
+++ circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float radius, area, peri;
+    printf("enter the radius of the circle: ");
+    scanf("%f", &radius);
+    area=3.14*radius*radius;
+    peri=2*3.14*radius;
+    printf("the area and perimeter of the circle is: %f and %f respectively.", area, peri);
+    return 0;
+}
