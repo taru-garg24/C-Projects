22e3c762f1bae634a74514fe545e095ed6423784
Taru <tarugg24@gmail.com>
2026-08-28 18:08:13 +0530

    add this program

---
+++ leap_year.c
@@ -0,0 +1,24 @@
+//program to find if the year is a leap year or not
+#include <stdio.h>
+int main(){
+    int year;
+    printf("enter a year: ");
+    scanf("%d", &year);
+    if (year%100==0){
+        if (year%400==0) {
+            printf("%d is a leap year", year);
+        }
+        else{
+            printf("%d is not a leap year", year);
+        }
+    }
+    else{
+        if (year%4==0){
+            printf("%d is a leap year", year);
+        }
+        else{
+            printf("%d is not a leap year", year);
+        }
+    }
+    return 0;
+}
