c902cac6c4f2f8d7df24da9ed195d1350589e20e
Taru <tarugg24@gmail.com>
2026-09-04 00:03:04 +0530

    post increament program added

---
+++ post_inc.c
@@ -0,0 +1,12 @@
+//program to print numbers after eusing pre and post increament operators
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter two numbers: ");
+    scanf("%d %d", &a,&b);
+    c=a++ + b;
+    printf("vlaues of a, b and c after post increament are: %d, %d, and %d\n", a,b,c);
+    return 0;
+
+}
