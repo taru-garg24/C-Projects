babe0e71dd43ab660c5a6a53b32e210869285a8d
Taru <tarugg24@gmail.com>
2026-09-06 23:30:32 +0530

    add multiplication program

---
+++ multiplication_table.c
@@ -0,0 +1,13 @@
+//Multiplication Table for a Given Integer
+#include <stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    for (i=1;i<=10;++i)
+    {
+        printf("%d * %d = %d\n", n,i,n*i);
+    }
+    return 0;
+}
